9a32490623f250027ef519bbe4a06cad672ececa
etykaken <etykaken@gmail.com>
2026-08-09 20:14:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ from services.ai_service import (
     ask_financial_assistant,
 )
 
-from services.supabase_service import test_connection
 
 from utils.security import redact_secret_like_strings
 from utils.transactions import validate_transactions
